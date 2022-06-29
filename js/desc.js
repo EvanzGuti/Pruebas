868d8379b8da182cc564def3e27de8d7ca3afe3f
EvanzGuti <84587330+EvanzGuti@users.noreply.github.com>
2022-06-29 17:44:55 -0400

    Add files via upload

---
+++ js/desc.js
@@ -0,0 +1,48 @@
+/* Variables */
+var nomani;
+var codcarp;
+var urlimg;
+var contenedorimagen = document.querySelector(".contimgdiv");
+
+/* Funciones */
+
+//funcion para recuperar el nombre del anime y el codigo de la carpeta
+//contenedora de las temporadas o peliculas
+function NombreCodigo() {
+  const valores = window.location.search;
+  const parametrosurl = new URLSearchParams(valores);
+  let nombre = parametrosurl.get("nomb");
+  const nom = nombre.split("'");
+  nomani = nom[1];
+  let codigo = parametrosurl.get("cod");
+  const cod = codigo.split("'");
+  codcarp = cod[1];
+}
+
+const imgrecup = async (nomani) => {
+  try {
+    //ingresando a la api de imagenes
+    const resPost = await axios(
+      "https://script.google.com/macros/s/AKfycbyFEOR8gOIJcpD9NKgFdpdKB29TgU1ZUjJZgrnet72TIrSu_-fHJxnaTxlbJtG3f-pYwA/exec"
+    );
+    //guardando en un vector todas las imagenes de la api
+    var imgvec = resPost.data.imagenes;
+    for (var i in imgvec) {
+      nombre = imgvec[i].nombre;
+      url = imgvec[i].url;
+      if (nombre == nomani) {
+        urlimg = url;
+      }
+    }
+    ponerimg(urlimg);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function ponerimg(urlimg) {
+  contenedorimagen.innerHTML = "<img src='" + urlimg + "'>";
+}
+
+NombreCodigo();
+imgrecup(nomani);
