9fbb734866deeb05a1cf84111ec7a8753925c53c
EvanzGuti <84587330+EvanzGuti@users.noreply.github.com>
2022-07-02 22:08:03 -0400

    Add files via upload

---
+++ js/desc.js
@@ -0,0 +1,43 @@
+var vectorimagenes = JSON.parse(localStorage.getItem("imagenesjson"));
+var nombre;
+var codigo;
+
+//funcion para recuperar el nombre del anime y el codigo de la carpeta
+//contenedora de las temporadas o peliculas
+function NombreCodigo() {
+  const valores = window.location.search;
+  const parametrosurl = new URLSearchParams(valores);
+  let nomb = parametrosurl.get("nomb");
+  const nom = nomb.split("'");
+  nombre = nom[1];
+  let codi = parametrosurl.get("cod");
+  const cod = codi.split("'");
+  codigo = cod[1];
+}
+
+//Sacando el nombre y el codigo
+NombreCodigo();
+
+//Funcion para sacar la url de la imagen
+function sacarurl(nombre) {
+  for (var i in vectorimagenes) {
+    nom = vectorimagenes[i].nombre;
+    if (nombre == nom) {
+      var url = vectorimagenes[i].url;
+      break;
+    }
+  }
+  return url;
+}
+
+function ponerimgnombre(nombre) {
+  var contenedorimagen = document.querySelector(".contimgdiv");
+  contenedorimagen.innerHTML =
+    "<img src='https://i.ibb.co/" +
+    sacarurl(nombre) +
+    "'><div class='titulo'><h3 class='text'>" +
+    nombre +
+    "</h3></div>";
+}
+
+ponerimgnombre(nombre);
